f7b7bc56bed9acdac18db4b52fb835bc375c2333
Riku <lushanyuan0@gmail.com>
2026-03-10 21:05:38 +0900

    PWA化を実装

--- script.js
+++ script.js
@@ -562,3 +562,15 @@ loadTasks();
 loadTheme();
 loadViewState();
 renderTasks();
+
+if("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker.register("./sw.js")
+            .then(function(registration) {
+                console.log("Service Worker registered:", registration);
+            })
+            .catch(function(error) {
+                console.log("Service Worker registration failed:", error);
+            });
+    });
+}
---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "todo-app-cache-v1";
+
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./image/favicon.ico",
+    "./image/apple-touch-icon-180x180.png",
+    "./image/icon-192.png",
+    "./image/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
